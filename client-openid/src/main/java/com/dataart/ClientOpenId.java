dea9d6aca1d313dea0b74ef6b34e4c4c8bdd4579
Serhii Soboliev <sergiy.sobolev@gmail.com>
2020-01-05 23:05:30 +0200

    OpenId authentication with google

---
+++ client-openid/src/main/java/com/dataart/ClientOpenId.java
@@ -0,0 +1,12 @@
+package com.dataart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOpenId {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientOpenId.class, args);
+    }
+}
